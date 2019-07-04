d5b23c21cfc69f15fd5506ff3a480dae81999cdb
Emil Pawlak <Emil.Szymon.Pawlak@gmail.com>
2019-07-04 19:45:48 +0200

    Updates

---
+++ precache-manifest.3bbc95e9a839c869234c29efb66fbaa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7732fc67caaff9f88291db2a9c8e4a9",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "f8b72b9b51661f4cd530",
+    "url": "/First-React-App/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/First-React-App/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f8b72b9b51661f4cd530",
+    "url": "/First-React-App/static/js/main.34e08974.chunk.js"
+  },
+  {
+    "revision": "032599635ed79cad8f9d",
+    "url": "/First-React-App/static/js/runtime~main.540e421f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/First-React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
